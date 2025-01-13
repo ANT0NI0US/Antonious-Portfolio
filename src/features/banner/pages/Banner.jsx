d3959224df54625ff4b01cb4ce4ce527bd7e4d85
antonious nasr <antoniousnasr3@gmail.com>
2025-01-13 22:58:00 +0200

    profile image

--- src/features/banner/pages/Banner.jsx
+++ src/features/banner/pages/Banner.jsx
@@ -14,11 +14,11 @@ export default function Banner() {
         {/* IMAGE SECTION */}
         <div className="flex w-full items-center justify-center lg:basis-3/5 lg:justify-end">
           <div
-            className="before:max-w-[400px] lg:relative lg:z-0 lg:ml-20 lg:before:absolute lg:before:-left-12 lg:before:top-2 lg:before:z-[-1]
-            lg:before:h-full lg:before:w-full lg:before:max-w-[450px] lg:before:rounded-full lg:before:border-2 2xl:before:max-w-[600px]"
+            className="before:max-w-[250px] sm:before:max-w-[400px] lg:relative lg:z-0 lg:ml-20 lg:before:absolute lg:before:-left-12 lg:before:top-2
+            lg:before:z-[-1] lg:before:h-full lg:before:w-full lg:before:max-w-[450px] lg:before:rounded-full lg:before:border-2 2xl:before:max-w-[600px]"
           >
             <img
-              className="w-full max-w-[400px] rounded-full transition duration-500 hover:grayscale hover:filter lg:max-w-[450px] 2xl:max-w-[600px]"
+              className="w-full max-w-[250px] rounded-full transition duration-500 hover:grayscale hover:filter sm:max-w-[400px] lg:max-w-[450px] 2xl:max-w-[600px]"
               src={profile}
               alt="profile"
             />
