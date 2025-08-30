269949a75419a763bebbc481aab2556d5679fb43
ANT0NI0US <antoniousnasr3@gmail.com>
2025-08-30 19:59:10 +0300

    add new certificate

---
+++ src/data/Certifications.js
@@ -0,0 +1,16 @@
+export const Certifications = [
+  {
+    id: 1,
+    date: "Aug 2024 - Mar 2025",
+    place: "Udemy",
+    title: "The Ultimate React Course 2024: React, Next.js, Redux & More",
+    href: "https://www.udemy.com/certificate/UC-d335d64a-0eea-4ae8-8ac4-488f47177db1/",
+  },
+  {
+    id: 2,
+    date: "Aug 2025 - Aug 2025",
+    place: "Udemy",
+    title: "React & TypeScript - The Practical Guide",
+    href: "https://www.udemy.com/certificate/UC-e82b621c-6bda-496f-9c6d-0d418a403040/",
+  },
+];
